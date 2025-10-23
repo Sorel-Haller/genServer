1b97e56b5e67658b06a4512b411df99a05a14959
Sorel-Haller <hallersorel@gmail.com>
2025-10-23 16:40:14 +0300

    bookRoutes

---
+++ src/routes/book.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/books', (request, response) => {});
+router.get('/books/:id', (request, response) => {
+    response.json({
+        id: request.params.id,
+        title: "Sample Book",
+    });        
+});
+router.post('/books', (request, response) => {});
+router.put('/books/:id', (request, response) => {});
+router.delete('/books/:id', (request, response) => {});
+
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bookRoutes from './routes/book.routes.js';
 
 const app = express();
 const PORT = 3000;
@@ -6,9 +7,14 @@ const PORT = 3000;
 //app.use(express.json());
 
 app.get('/welcome', (request, response) => { //päring jõuab ükskord serverini ja saab vastuse
-    response.send('Welcome to the Server!');
+    response.send({
+        message:'Welcome to the Server!',
+    });
 });
+
+app.use(bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
