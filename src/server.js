d6bfc4c7a0812d12b9fd4fdcb20af9ab73d956c1
Sorel-Haller <hallersorel@gmail.com>
2025-10-23 15:16:50 +0300

    kommentaarid

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+//app.use(express.json());
+
+app.get('/welcome', (request, response) => { //päring jõuab ükskord serverini ja saab vastuse
+    response.send('Welcome to the Server!');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
