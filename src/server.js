fd54a79df214cd341b9316d0a2945ea00534973d
Sorel-Haller <hallersorel@gmail.com>
2025-10-24 11:25:22 +0300

    viimne 24.10

--- src/server.js
+++ src/server.js
@@ -1,34 +1,22 @@
-// src/server.js
 import express from 'express';
 import bookRoutes from './routes/book.routes.js';
-import db from './config/db.js'; 
+import db from './config/db.js';
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.use(express.json());
+//app.use(express.json());
 
-(async () => {
-  try {
-    const [rows] = await db.execute('SELECT NOW() AS `current_time`');
-    
-    if (rows.length > 0) {
-      console.log('✅ Connected to MySQL. Current time:', rows[0].current_time);
-    } else {
-      console.log('⚠️ Query returned no results');
-    }
-  } catch (error) {
-    console.error('❌ Database connection failed:', error.message);
-    process.exit(1); 
-  }
+app.get('/welcome', (request, response) => { //päring jõuab serverini ja saab vastuse
+    response.send({
+        message:'Welcome to the Server!',
+    });
+});
 
-  app.get('/welcome', (req, res) => {
-    res.send({ message: 'Welcome to the Server!' });
-  });
+app.use(bookRoutes);
 
-  app.use('/books', bookRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-  app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-  });
-})();
