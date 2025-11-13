3759f06f89b5e81ccfbeba6fd1be358f36e53e08
Sorel-Haller <hallersorel@gmail.com>
2025-11-13 17:08:06 +0200

    kommentaarid middleware's

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -1,3 +1,5 @@
+//middleware takes in request, response and next and "does something with it (like here we validate the request body) 
+// if it validates it then it sends on and if not then sends back an error response"
 export const validate = (schema) => (request, response, next) => {
 
     const { error } = schema.validate(request.body, { 
@@ -17,8 +19,11 @@ export const validate = (schema) => (request, response, next) => {
         }, {});
         
         return response.status(400).json({          // tagastame 400 Bad Request koos veateadetega
-            message: typeof error === "ValidationError" ? errors.message : 'Validation failed',
+            message: typeof error === "ValidationError" ? errors.message : 'Validation failed', 
             errors: { ...errorBag }
         });
     }
+
+    next(); // kui valideerimine õnnestus, kutsume järgmise middleware'i või kontrolleri, request can go further
+
 };
