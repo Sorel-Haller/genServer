2916db2c187fcaee27b1d5ec78a9c23cb61c7a75
Sorel-Haller <hallersorel@gmail.com>
2025-11-13 17:03:34 +0200

    validate middleware code

---
+++ src/middleware/validate.middleware.js
@@ -0,0 +1,24 @@
+export const validate = (schema) => (request, response, next) => {
+
+    const { error } = schema.validate(request.body, { 
+        abortEarly: false,
+        errors: {
+            wrap: {
+                label: 'false'
+            }
+        }
+    });
+
+    if (error) {
+        const errorBag = errors.details.reduce((accumulator, detail) => { // kogume kõik veateated ühte objekti
+            const field = detail.path.join('.');        // võtame välja veateate välja nime
+            accumulator[field] = detail.message;        // lisame veateate akumulaatorisse
+            return accumulator;
+        }, {});
+        
+        return response.status(400).json({          // tagastame 400 Bad Request koos veateadetega
+            message: 'Validation failed',
+            errors: errorBag
+        });
+    }
+};
--- src/validations/book.validation.js
+++ src/validations/book.validation.js
@@ -1,6 +1,6 @@
 import Joi, { any } from "joi";
 
-const bookSchema = Joi.object({                             // rules for the title, description, thumbnail_url and release_year
+export const bookSchema = Joi.object({                             // rules for the title, description, thumbnail_url and release_year
     title: Joi.string().max(255).required(),
     description: Joi.string().required(),
     thumbnail_url: Joi.string().uri().required(),           
