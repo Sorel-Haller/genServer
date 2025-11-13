15a5d1563a719be508c6774624841cedf1b97a36
Sorel-Haller <hallersorel@gmail.com>
2025-11-13 17:05:14 +0200

    validation error

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -17,8 +17,8 @@ export const validate = (schema) => (request, response, next) => {
         }, {});
         
         return response.status(400).json({          // tagastame 400 Bad Request koos veateadetega
-            message: 'Validation failed',
-            errors: errorBag
+            message: typeof error === "ValidationError" ? errors.message : 'Validation failed',
+            errors: { ...errorBag }
         });
     }
 };
