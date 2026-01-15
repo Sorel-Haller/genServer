5724f974e1882a0ddc818a3220db9cd35056c00b
Sorel-Haller <hallersorel@gmail.com>
2026-01-15 17:05:37 +0200

    Fix typo in ExistingEntityError class name

--- src/utils/ExistingEntityError.js
+++ src/utils/ExistingEntityError.js
@@ -2,6 +2,10 @@ import AppError from "./AppError.js"
 
 class ExistsingEntityError extends AppError {
   constructor(message) {
-    
+    super(message, 401);
+
   }
 }
+
+export default ExistingEntityError;
+
