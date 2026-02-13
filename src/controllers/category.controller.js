3161943ed584d594cd24cb81a2d3dacbc66790aa
Sorel-Haller <hallersorel@gmail.com>
2026-02-13 11:16:06 +0200

    fixed category routes

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -33,109 +33,33 @@ class CategoryController {
             next(exception)
         }
     }
-};
 
-export const index = async (request, response, next) => {
-    try {
-        const Builder = new QueryBuilder(request.query, {
-            defaultSort: 'created_at',
-            defaultTake: 50,
-            allowedSearchFields: ['name'],
-            allowedIncludes: {
-                'books': { include: { book: true } }
-            }
-        });
-
-        const [categories, count] = await Promise.all([
-            prisma.category.findMany(prismaQuery),
-            prisma.category.count({ where: prismaQuery.where })
-        ]);
-
-        const meta = Builder.getPaginationMeta(count);
-
-        response.status(200).json({
-            message: 'All categories',
-            data: categories,
-            meta
-        });
-    } catch (exception) {
-        next(exception);
-    }
-};
-
-export const create = async (request, response, next) => {
-    try {
-        const { name } = request.body;
-
-        if (!name) {
-            throw new ValidationError("Category name is required", {
-                field: "name"
+    async update(request, response, next) {
+        try {
+            const{ id } = request.params
+            const{ name} = request.body
+            const updatedEntity = await this.categoryService.updateCategory(id, name);
+            response.status(200).json({
+                message: 'Category updated',
+                updatedEntity
             });
+        } catch (exception) {
+            next(exception)
         }
-
-        const createdCategory = await prisma.category.create({
-            data: { name }
-        });
-
-        response.status(201).json({
-            message: 'Category created',
-            data: createdCategory
-        });
-
-    } catch (exception) {
-        next(exception);
     }
-};
 
-export const update = async (request, response, next) => {
-    try {
-        const { id } = request.params;
-        const { name } = request.body;
-
-        const existingCategory = await prisma.category.findUnique({
-            where: { id: Number(id) }
-        });
-
-        if (!existingCategory) {
-            throw new NotFoundError(`Category with id ${id} not found`);
+    async destroy(request, response, next) {
+        try {
+            const{ id } = request.params
+            await this.categoryService.deleteCategory(Number(id));
+            response.status(200).json({
+                message: 'Category deleted',
+            });
+        } catch (exception) {
+            next(exception)
         }
-
-        const updatedCategory = await prisma.category.update({
-            where: { id: Number(id) },
-            data: { name }
-        });
-
-        response.status(200).json({
-            message: 'Category updated',
-            data: updatedCategory
-        });
-
-    } catch (exception) {
-        next(exception);
     }
-};
+}
 
-export const destroy = async (request, response, next) => {
-    try {
-        const { id } = request.params;
+export default CategoryController;
 
-        const existingCategory = await prisma.category.findUnique({
-            where: { id: Number(id) }
-        });
-
-        if (!existingCategory) {
-            throw new NotFoundError(`Category with id ${id} not found`);
-        }
-
-        await prisma.category.delete({
-            where: { id: Number(id) }
-        });
-
-        response.status(200).json({
-            message: 'Category deleted'
-        });
-
-    } catch (exception) {
-        next(exception);
-    }
-};
